0a47db47a0694e43edced3b00311b1cafb0476fc
rocky <rocky@memoio.io>
2024-11-27 16:16:35 +0800

    first commit

---
+++ dumper/abi.go
@@ -0,0 +1,9 @@
+package dumper
+
+import (
+	"github.com/grid/contracts/go/market"
+	"github.com/grid/contracts/go/registry"
+)
+
+var RegisterABI = registry.RegistryABI
+var MarketABI = market.MarketABI
